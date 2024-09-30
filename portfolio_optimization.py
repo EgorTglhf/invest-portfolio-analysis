c406eb85ca6d16e0702bce79b8b85af7a1a5c701
Egor Tykmanov <egor.tykmanov@glowbyteconsulting.com>
2024-09-30 23:15:15 +0300

    analysis custom portfolio by generating random portfolios and choosing max sharpe ratio

--- portfolio_optimization.py
+++ portfolio_optimization.py
@@ -11,7 +11,7 @@ def return_portfolios(expected_returns, cov_matrix):
     selected = (expected_returns.axes)[0]
     
     num_assets = len(selected) 
-    num_portfolios = 5000
+    num_portfolios = 50000
     
     for single_portfolio in range(num_portfolios):
         weights = np.random.random(num_assets)
